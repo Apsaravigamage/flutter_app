e1a4e462a6490653f6a90eb52a236019d621b6a0
Apsaravi Gamage <apsaravigamage@gmail.com>
2020-09-02 19:09:03 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('My First App'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+        body: Center(
+          child: Text(
+            'Welcome !',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey,
+              fontFamily: 'IndieFlower',
+          ),
+
+          ),
+        ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('click'),
+        backgroundColor: Colors.red,
+      ),
+    ),
+  ));
+
+
